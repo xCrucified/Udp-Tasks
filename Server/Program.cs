5873e0b700d9e1ceaf67925c1d9ed556f90a261d
zheni4ka <ebranik15@gmail.com>
2023-10-16 00:03:25 +0300

    123

---
+++ Server/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        static List<KeyValuePair<string, List<string>>> m_key = new List<KeyValuePair<string, List<string>>>()
+        {
+            new KeyValuePair<string, List<string>>("Hello", new List<string>(){"Hi!", "Hey!", "What`s up!"}),
+            new KeyValuePair<string, List<string>>("What time is it?", new List<string>(){$"It`s {DateTime.Now}"}),
+            new KeyValuePair<string, List<string>>("What date is today", new List<string>(){$"Today its {DateTime.Now.Year + " " + DateTime.Now.Day + " " + DateTime.Now.Month}"}),
+        };
+
+
+        static string address = "127.0.0.1"; // поточний адрес
+        static int port = 8080;              // порт для приема входящих запросов
+
+        static void Main(string[] args)
+        {
+            // получаем адреса для запуска сокета
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+            // об'єкт для отримання адреси відправника
+            IPEndPoint remoteEndPoint = null;
+
+            UdpClient listener = new UdpClient(ipPoint);
+
+            try
+            {
+                Console.WriteLine("Server started! Waiting for connection...");
+
+                while (true)
+                {
+                    byte[] data = listener.Receive(ref remoteEndPoint);
+
+                    string msg = Encoding.Unicode.GetString(data);
+
+                    if (m_key.Where(x => x.Key == msg).Select(x => x.Key).Count() != 0)
+                    {
+
+                        var tmp = m_key.Where(x => x.Key == msg).Select(x => x.Value).ToArray();
+
+                        string a = tmp[new Random().Next(1, tmp.Length)].First();
+                        data = Encoding.Unicode.GetBytes(a);
+                        listener.Send(data, data.Length, remoteEndPoint);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            listener.Close();
+        }
+
+    }
+}
--- UPD/MainWindow.xaml.cs
+++ UPD/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +14,67 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PropertyChanged;
 
 namespace UPD
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    public class MessageShow
+    {
+        public string message;
+        public DateTime time;
+
+        public MessageShow(string message, DateTime time)
+        {
+            this.message = message;
+            this.time = time;
+        }
+
+        public string str => $"{message}\t\t\t\t{time.Hour}:{time.Minute}";
+    }
+
+
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void SendBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(IpBox.Text), Convert.ToInt32(PortBox.Text));
+
+                IPEndPoint remoteIpPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                UdpClient client = new UdpClient();
+
+                string message = MessageBox.Text;
+                
+                byte[] data = Encoding.Unicode.GetBytes(message);
+
+                client.Send(data, data.Length, ipPoint);
+
+                data = client.Receive(ref remoteIpPoint);
+
+                Encoding.Unicode.GetString(data);
+
+                ChatList.Items.Add(Encoding.Unicode.GetString(data) + "  -  "+ DateTime.Now);
+                
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
     }
-}
+ }
+
